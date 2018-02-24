771330d57799bf19fda936a2521d76ce3add006b
Juan Jimeno <jimenojmm@gmail.com>
2018-02-24 10:34:40 +0800

    added comments

--- lambda/app/index.js
+++ lambda/app/index.js
@@ -4,8 +4,8 @@ const PubNub = require("pubnub");
 
 const pubnub = new PubNub({
     ssl: true,
-    publish_key: "pub-c-",
-    subscribe_key: "sub-c-"
+    publish_key: "pub-c-", // PLACE YOUR PUBNUB PUBLISH KEY HERE
+    subscribe_key: "sub-c-" // PLACE YOUR PUBNUB SUBSCRIBE KEY HERE
 });
 
 exports.handler = function(event, context, callback) {
@@ -14,27 +14,40 @@ exports.handler = function(event, context, callback) {
     alexa.execute();
 };
 
+//this are the responses for each Intent. Task ID will be the index of each reponse in the array
 const responseArr = [
     'Hi, You can ask me to feed the fish, get you a soda, or check out what\'s happening in the house!',
     'Okay! Moving the robot to feed the fish',
     'Okay! Moving the robot to get you a soda',
     'Okay! Moving the robot to the living room'
+    //Append your own response here
 ];
 
 const handlers = {
     'LaunchRequest': function () {
+        //this is O'nine's default response when the skill is called yet no Intent matched
         const speechOutput = responseArr[0];
         this.response.speak(speechOutput);
     },
     'FeedFishIntent': function () {
+        //tell MQTT broker that user wants to feed the fish 
+        //send task id 1
         this.emit('SendToRobot', 1);
     },
     'GetSodaIntent': function () {
+        //tell MQTT broker that user wants to a soda
+        //send task id 1
         this.emit('SendToRobot', 2);
     },
     'SnapLivingRoomIntent': function () {
+        //tell MQTT broker that user wants to check out what's up in the living room
+        //send task id 1
         this.emit('SendToRobot', 3);
     },
+    // 'AdditionalIntent': function () {
+    //     //this is a sample function how to add your additional Intent
+    //     this.emit('SendToRobot', 4); // remember to increment the task id as you add more 
+    // },
     'AMAZON.HelpIntent': function () {
         const speechOutput = 'Hello I am Onine';
         const reprompt = 'Say hello, to hear me speak.';
@@ -51,16 +64,20 @@ const handlers = {
         this.emit(':responseReady');
     },
     'SendToRobot': function (taskID) {   
+        // create the payload that will be sent to the MQTT broker
         var pubConfig = {
             channel: "onine",
             message : {
+                //pass the index number of the Intent as task id
                 "action" : taskID
             }
         };
    
+        //send the task id to the MQTT broker
         pubnub.publish(pubConfig)
             .then(
                 (response) => {
+                    //send a respone to the Alexa skill if MQTT acknowledge
                     console.log(response);
                     const speechOutput = responseArr[taskID];
                     this.emit(':tell',speechOutput);
